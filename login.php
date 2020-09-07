09549f9fd40398836f07b7dbcea9c4196b2eb523
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-07 16:37:46 +0300

    More styling with css files.

--- login.php
+++ login.php
@@ -3,29 +3,36 @@
 <html>
 <head>
     <title>Reistration system</title>
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="styles/login.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 </head>
 <body>
-    <div class="header">
-        <h2>Login</h2>
+    <div class="login">
+        <div>
+            <h2>Login</h2>
+        </div>
+        <form method="post" action="login.php">
+            <?php include('errors.php');  ?>
+            <div class="inputs">
+                <label for="username">
+                    <i class="fas fa-user"></i>
+                </label>
+                <input type="text" name="username" placeholder="Username" id="username">
+            </div>
+            <div class="inputs">
+                <label for="password">
+                    <i class="fas fa-lock"></i>
+                </label>
+                <input type="password" name="password" placeholder="Password" id="password"><br>
+            
+            
+                <button type="submit" class="btn" name="user_login">Login</button>
+            </div>
+            <p>
+                Not yet a member? <a href="register.php">Sign up</a>
+            </p>
+        </form>
     </div>
-    <form method="post" action="login.php">
-        <?php include('errors.php');  ?>
-        <div class="inputs">
-            <label>Username</label>
-            <input type="text" name="username">
-        </div>
-        <div class="inputs">
-            <label>Password</label>
-            <input type="password" name="password">
-        </div>
-        <div class="inputs">
-            <button type="submit" class="btn" name="user_login">Login</button>
-        </div>
-        <p>
-            Not yet a member? <a href="register.php">Sign up</a>
-        </p>
-    </form>
 
 </body>
 </html>
