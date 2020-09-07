10bd6ab65ab76bd39c5ab1d0b2e85e1dc6a5d717
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-07 16:35:24 +0300

    Logout/view profile functionality.
    Prevent going loading back to  a profile that has been logged out already

--- index.php
+++ index.php
@@ -1,44 +1,57 @@
 <?php 
+
     session_start();
+    
     //check if user is already logged in
-    if(isset($_SESSION['username'])){
+    if(!isset($_SESSION['username'])){
         $_SESSION['msg'] = 'You must first log in!';
-        header('login.php');
+        header('location:login.php');
     }
     //check if logout button has been clicked;
     if(isset($_GET['logout'])){
         session_destroy();
         unset($_SESSION['username']);
-        header('location:login.php');
+        header('location:logout.php');
     }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="styles/index.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 </head>
-<body>
-    <div class="header">
-        <h2>Home page</h2>
-    </div>
+<body class="loggedin">
+    <nav class="navtop">
+       <div>
+            <h1>My website</h1>
+            <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+            <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+        </div> 
+    </nav>
+
     <div class="content">
-        <?php if (isset($_SESSION['success'])) : ?>
+            <h2>Home Page</h2>
+            <p>Welcome back, <?=$_SESSION['username']?>!</p>
+    </div>
+    <!--<div class="content">
+
+        <?php //if (isset($_SESSION['success'])) : ?>
       <div class="error success" >
         <h3>
           <?php 
-            echo $_SESSION['success']; 
-            unset($_SESSION['success']);
+            //echo $_SESSION['success']; 
+            //unset($_SESSION['success']);
           ?>
         </h3>
       </div>
-    <?php endif ?>
+    <?php //endif ?>-->
 
     <!-- logged in user information -->
-    <?php  if (isset($_SESSION['username'])) : ?>
-        <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    <!--<?php  //if (isset($_SESSION['username'])) : ?>
+        <p>Welcome <strong><?php //echo $_SESSION['username']; ?></strong></p>
         <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-    <?php endif ?>
-    </div>
+    <?php //endif ?>-
+    </div>-->
 </body>
 </html>
